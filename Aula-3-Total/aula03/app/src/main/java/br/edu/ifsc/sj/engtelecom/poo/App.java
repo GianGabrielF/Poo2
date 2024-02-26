109a508fa03ab9bd9c81937712668203e254a299
GG <gian.s12@aluno.ifsc.edu.br>
2024-02-26 11:28:36 -0300

    Aula03

---
+++ Aula-3-Total/aula03/app/src/main/java/br/edu/ifsc/sj/engtelecom/poo/App.java
@@ -0,0 +1,13 @@
+package br.edu.ifsc.sj.engtelecom.poo;
+
+    public class App{
+
+
+    public static void main(String[] args) {
+    
+        System.out.println("Olá mundo");
+
+    }
+    
+    
+}
---
+++ Aula-3-Total/aula03/app/src/test/java/br/edu/ifsc/sj/engtelecom/poo/AppTest.java
@@ -0,0 +1,10 @@
+
+package br.edu.ifsc.sj.engtelecom.poo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test void appHasAGreeting() {
+    }
+}
