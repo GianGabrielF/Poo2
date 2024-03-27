1c3491267c8504c4b244c911ebcb8a3e382961fe
gg <gian.s12@aluno.ifsc.edu.br>
2024-03-27 11:16:29 -0300

    aula 10 e ex4

--- laboratorio01/ex04/app/src/main/java/engtelecom/poo/Data.java
+++ laboratorio01/ex04/app/src/main/java/engtelecom/poo/Data.java
@@ -84,7 +84,64 @@ public class Data {
         return String.format("%02d/%02d/%04d",this.dia,this.mes,this.ano);
     }
     
+    public String porExtenso(){
+        String[] mes = {"janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"};
+
+        return this.dia + " de " + mes[this.mes] + " de " + this.ano;
+    }    
+
+    private int comparaData(Data d1,Data d2){
         
+        if(d1.getAno()>d2.getAno()){
+            return 1;//d1>d2
+        }
+        if(d1.getAno()<d2.getAno()){
+            return -1;//d1<d2
+        }
+        if(d1.getMes()>d2.getMes()){
+            return 1;
+        } 
+        if(d1.getMes()<d2.getMes()){
+            return -1;
+        }
+        if(d1.getDia()>d2.getDia()){
+            return 1;
+        }
+        if(d1.getDia()<d2.getDia()){
+            return -1;
+        }
+        return 0;//d1==d2
+
+    }
+
+    public int diffDias(Data d2){
+        int dif;
+
+        if(this.mes == d2.getMes() && this.ano == d2.getAno()){
+            return Math.abs(this.dia - d2.getDia());
+        } else if (this.ano == d2.getAno()){
+            if(comparaData(this, d2)==1){
+                dif = mesDias(d2.getMes()) - d2.getDia();
+                for(int i= d2.getMes()+1; i<this.mes;i++){
+                    dif+=mesDias(i);
+                }
+                dif+=this.dia;
+                return dif;
+            } else {
+                if(comparaData(this, d2)==-1){
+                    dif = mesDias(this.mes) - this.dia;
+                    for(int i= this.mes+1; i<d2.getMes();i++){
+                        dif+=mesDias(i);
+                    }
+                    dif+=d2.getDia();
+                    return dif;
+                }
+            }
+
+        } else {
+            if
+        }
+    }
 
     
 
