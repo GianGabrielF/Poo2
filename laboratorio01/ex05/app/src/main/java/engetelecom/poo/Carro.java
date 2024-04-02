35836113fe4d0b1e2047c654258e93b0827597d9
gg <gian.s12@aluno.ifsc.edu.br>
2024-04-02 10:59:10 -0300

    comecado ex05

---
+++ laboratorio01/ex05/app/src/main/java/engetelecom/poo/Carro.java
@@ -0,0 +1,11 @@
+package engetelecom.poo;
+
+public class Carro {
+
+    private static int nCarros;
+    private int id;
+    private double velocidade;
+    private String marca;
+    private final double V_MAX=200.0;
+    
+}
